04250b7ae5185b1ce3d881a876df347be04d5442
prodigeris <arnas.kazlauskas@kilo.health>
2024-03-13 00:31:43 +0200

    feat: optimized sol

--- 746-min-cost-climbing-stairs/min-cost.py
+++ 746-min-cost-climbing-stairs/min-cost.py
@@ -6,19 +6,10 @@ class Solution:
         self.cache = {}
 
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        return self.checkCost(0, cost)
+        for i in range(len(cost) - 3, -1, -1):
+            cost[i] = min(cost[i+1], cost[i+2])
+        return min(cost[0], cost[1])
 
-    def checkCost(self, i: int, cost: List[int]) -> int:
-        if len(cost) <= 1:
-            return 0
-        if i in self.cache:
-            return self.cache[i]
-
-        l = self.checkCost(i+1, cost[1:])
-        r = self.checkCost(i+2, cost[2:])
-        cost = min(cost[0] + l, cost[1] + r)
-        self.cache[i] = cost
-        return cost
 
 
 
